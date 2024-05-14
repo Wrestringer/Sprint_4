8f0684c174c802a1da16c2b3644e6e01872b82e2
Danil Martyushev <martyusheff.danil@yandex.ru>
2024-05-14 13:47:05 +0300

    added constants

---
+++ src/main/java/ru/praktikumServices/QaScooter/constants/Color.java
@@ -0,0 +1,6 @@
+package ru.praktikumServices.QaScooter.constants;
+
+public class Color {
+    public static final String COLOR_GREY = "grey";
+    public static final String COLOR_BLACK = "black";
+}
---
+++ src/main/java/ru/praktikumServices/QaScooter/constants/Result.java
@@ -0,0 +1,5 @@
+package ru.praktikumServices.QaScooter.constants;
+
+public class Result {
+    public static final String RESULT_ORDER_SCOOTER_POSITIVE = "Заказ оформлен";
+}
